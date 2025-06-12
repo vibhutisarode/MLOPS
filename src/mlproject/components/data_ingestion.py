f101ce5eab56baf2b7cede143f4dfea642c6cb35
vibhuti sarode <vibhutisarode00@gmail.com>
2025-06-13 00:07:13 +0530

    data injection

---
+++ src/mlproject/components/data_ingestion.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+from src.mlproject.utils import read_sql_data
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'raw.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        try:
+            logging.info("Starting data ingestion from MySQL")
+            
+            # Read data from MySQL using the utility function
+            df = read_sql_data()
+            logging.info("Data successfully fetched from MySQL")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            # Save raw data
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            # Split the data
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            # Save split data
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data Ingestion completed and files saved")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e, sys)
