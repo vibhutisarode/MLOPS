dba34ec0ed8f4f9390cbd2fd751b738c71b9a68c
vibhuti sarode <vibhutisarode00@gmail.com>
2025-06-16 16:42:35 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 #end to end data science project
+import dagshub
+dagshub.init(repo_owner='vibhutisarode', repo_name='MLOPS', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- src/mlproject/exception.py
+++ src/mlproject/exception.py
@@ -1,19 +1,25 @@
 import sys
 from src.mlproject.logger import logging
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
 
-    return error_message
+def error_message_detail(error, error_detail: sys):
+    """
+    Returns detailed error message with traceback information.
+    If no active exception (i.e., sys.exc_info() returns None), fallback to basic message.
+    """
+    exc_type, exc_value, exc_tb = sys.exc_info()
+
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        return f"Error occurred in script: [{file_name}] at line number: [{exc_tb.tb_lineno}] error message: [{str(error)}]"
+    else:
+        return f"Error message: [{str(error)}]"
 
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_details:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_details)
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
